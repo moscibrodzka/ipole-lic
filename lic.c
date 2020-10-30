5ee232a77c39df41ff2e1d4f0a7bf9f8d1ee4030
moscibrodzka <m.moscibrodzka@astro.ru.nl>
2020-10-30 16:50:38 +0100

    Add files via upload

--- lic.c
+++ lic.c
@@ -6,7 +6,6 @@
 
 double imageS[NX][NY][NDIM];
 double image[NX][NY];
-double imageb[NX][NY];
 double imageI[NX][NY];
 double vx[NX][NY];
 double vy[NX][NY];
@@ -56,20 +55,26 @@ int main(int argc, char *argv[]){
     fclose(fp);
 
     fprintf(stdout,"read %s \n",argv[1]);
+
+
     
     for (int i = 0; i < NX; i++)
         for (int j = 0; j < NY; j++)
 	    image[i][j] = pow(imageS[i][j][0],1.0);
+	    //image[i][j] = sqrt(pow(imageS[i][j][1],2.0)+pow(imageS[i][j][2],2.0));
     make_ppm(image, freq, "image_fnu.ppm");
     fprintf(stdout,"made image_fnu.ppm \n");
-        
+
+    
     //in [uas]
     for (int i = 0; i < NX; i++){
 	x[i]=-FOV/2. + (i+0.5)*dx;
 	y[i]=-FOV/2. + (i+0.5)*dy;	
     }
     
-    double amp,ang;
+    double amp,ang,Imax,QUmax;
+    Imax=0.0;
+    QUmax=0.0;
     //construct a vector field for each pixel
     for (int i = 0; i < NX; i++){
         for (int j = 0; j < NY; j++){
@@ -78,34 +83,25 @@ int main(int argc, char *argv[]){
 	    vx[i][j] = (-sin(ang)*amp);
 	    vy[i][j] = ( cos(ang)*amp);
 	    image[i][j]=0.0; 
-	    
-	    //here one has many options for the background intensity, now set to ~P^2
+	    //here one has many options for the background intensity
 //	    imageI[i][j]=rand();// uniform 
-//	    imageI[i][j]=imageS[i][j][0]*rand(); // brightness ~ I
-	    imageI[i][j]=(imageS[i][j][1]*imageS[i][j][1]+imageS[i][j][2]*imageS[i][j][2])*rand(); // ~P^2
-//	    imageI[i][j]=sqrt(imageS[i][j][1]*imageS[i][j][1]+imageS[i][j][2]*imageS[i][j][2])*rand();// ~P
+	    imageI[i][j]=imageS[i][j][0]*rand(); // brightness ~ I
+//	    imageI[i][j]=imageS[i][j][0]; // brightness ~ I
+//	    imageI[i][j]=(imageS[i][j][1]*imageS[i][j][1]+imageS[i][j][2]*imageS[i][j][2])*rand(); // ~P^2
+//	    imageI[i][j]=sqrt(imageS[i][j][1]*imageS[i][j][1]+imageS[i][j][2]*imageS[i][j][2])*rand();// ~(P)
 //	    imageI[i][j]=rand()*pow(imageS[i][j][0],0.1); //some power of I
-
 	}
     }
 
-    
-
     // define length scale on the files L in uas to integrate over
-    double L=10.;//4.;//3;//10.;//80.; //uas  //parameter, the larger the number the smoother images
-
-    
-
-    //parameters below are fixed
-
+    double L=20.;//4.;//3;//10.;//80.; //uas  //parameter, the larger the number the smoother images
     double dl=0.01; //stepsize in uas in x and y
     double dlxy=sqrt(2.)*dl;
     
-#pragma omp parallel for schedule(dynamic,1) collapse(2) shared(x,y,vx,vy,imageI,L,dl)
-    for (int i = 0; i < NX; i++){
+#pragma omp parallel for schedule(dynamic,1) collapse(2) shared(x,y,vx,vy,imageI,dl,L)
+    for (int i = 0; i < NX-10; i++){
         for (int j = 0; j < NY; j++){
-	    	    
-	    double totl=0.0;
+	    double totl1=0.0;
 	    //starting point for position and velocity in the pixel center
 	    double x_line=x[i];
 	    double y_line=y[j];
@@ -114,48 +110,76 @@ int main(int argc, char *argv[]){
 	    double Ilocal;
 	    double h;
 	    double hsum=0.0;
-	    double LP=sqrt(imageS[i][j][1]*imageS[i][j][1]+imageS[i][j][2]*imageS[i][j][2])/imageS[i][j][0];
-	    while(totl < 0.5*L && x_line>startx && y_line>starty && x_line<stopx && y_line<stopy){
-		x_line += vx_line*dl;
-		y_line += vy_line*dl;
-		totl += dlxy;
+            //integrate forward until we hit boundary of image or integration limit
+	    while(totl1 < 0.5*L){
+	    	x_line += vx_line*dl;
+	    	y_line += vy_line*dl;
+		if(x_line < startx && y_line < starty && x_line > stopx && y_line > stopy) break;
+	    	totl1 += dlxy;
 		Ilocal=interp_scalar2d(x_line,y_line,imageI);
 		h=1./L;
-		//here one can use another filter
-		//h=hanning_windowed(totl,totl+dlxy);
+	    	//here one can use another filter
+	    	//h=hanning_windowed(totl,totl+dlxy);
 		hsum += h;
 		image[i][j] += Ilocal*h;
 		vx_line=interp_scalar2d(x_line,y_line,vx);
-		vy_line=interp_scalar2d(x_line,y_line,vy);
+	    	vy_line=interp_scalar2d(x_line,y_line,vy);
 	    }
-	    totl=0.0;
+	    double totl2=0.0;
 	    //starting point for position and velocity
 	    x_line=x[i];
 	    y_line=y[j];
 	    vx_line=vx[i][j];
 	    vy_line=vy[i][j];
-	    while(totl < 0.5*L && x_line>startx && y_line>starty && x_line<stopx && y_line<stopy){
-		x_line += -vx_line*dl;
-		y_line += -vy_line*dl;
-		totl += dlxy;
-		Ilocal=interp_scalar2d(x_line,y_line,imageI);
-		h=1./L;
-		//here one can use another filter
-		//h=hanning_windowed(totl,totl+dlxy);
+	    while(totl2 < 0.5*L){
+	    	x_line += -vx_line*dl;
+	    	y_line += -vy_line*dl;
+		if(x_line < startx && y_line < starty && x_line > stopx && y_line > stopy) break;
+	    	totl2 += dlxy;
+	    	Ilocal=interp_scalar2d(x_line,y_line,imageI);
+	    	h=1./L;
+	    	//here one can use another filter
+	    	//h=hanning_windowed(totl,totl+dlxy);
 		hsum+=h;
 		image[i][j] += Ilocal*h;
-		vx_line=interp_scalar2d(x_line,y_line,vx);
-		vy_line=interp_scalar2d(x_line,y_line,vy);
+	    	vx_line=interp_scalar2d(x_line,y_line,vx);
+	    	vy_line=interp_scalar2d(x_line,y_line,vy);
 	    }
-	    //here I am cutting out some edges with junk
-	    if(i>NX-10){
-		image[i][j]=0.0;
-	    } else{
-		image[i][j]/=hsum;
-	    }
-	
+	    image[i][j]/=hsum;
 	}
     }
+
+    //normalize both images to add them up and somewhat conserve the flux
+    double image_max=0.0;
+    double image_max2=0.0;
+    double QU_max2=0.0;
+    for (int i = 0; i < NX; i++){
+        for (int j = 0; j < NY; j++){
+	    if(image[i][j] > image_max) image_max=image[i][j];
+	    if(imageS[i][j][0] > image_max2) image_max2=imageS[i][j][0];
+	    if(imageS[i][j][1]*imageS[i][j][1]+imageS[i][j][2]*imageS[i][j][2] > QU_max2){
+		QU_max2=imageS[i][j][1]*imageS[i][j][1]+imageS[i][j][2]*imageS[i][j][2];
+	    }
+	}
+    }
+
+    for (int i = 0; i < NX; i++){
+        for (int j = 0; j < NY; j++){
+	    image[i][j]=image[i][j]/image_max;
+	    imageS[i][j][0]=imageS[i][j][0]/image_max2;
+	}
+    }
+
+    //add weight of stroke
+    double fac;
+    for (int i = 0; i < NX; i++){
+        for (int j = 0; j < NY; j++){
+	    //press brush hard only in regions with high pol flux
+	    fac=(imageS[i][j][1]*imageS[i][j][1]+imageS[i][j][2]*imageS[i][j][2])/(QU_max2);
+	    image[i][j] = imageS[i][j][0]*(1.-fac) + image[i][j]*fac;
+	}
+    }
+    
     
     make_ppm(image, freq, "image_lic.ppm");
     fprintf(stdout,"made image_lic.ppm \n");
